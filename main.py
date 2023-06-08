3f1f97164e4b17dd6e1da0663d4790fe494b99d7
liujiaozhiren <sijianing2008@icloud.com>
2023-06-08 15:52:38 +0800

    ...

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ d = np.random.rand(M)
 v = np.random.rand(N)
 MaxB_A = b_a.max() * 2
 maxr = max(d.max(), v.max())
-
+max_target = 0.0
 print(d, v, MaxB_A, maxr)
 
 
@@ -47,6 +47,7 @@ def restrain2(x_r):
 
 
 def opt():
+    global max_target
     # 创建优化器
     opt = Optimizer(dimensions=[(0.0, maxr)] * (N * M))
     while True:
@@ -58,17 +59,19 @@ def opt():
                 # print(restrain2(x), restrain1(x), x)
                 # 计算目标函数的值
                 y = objective(x)
-                #tq_str = f'output:{-y:.5f}, re1:{restrain1(x):.3f}, re2:{restrain2(x):.3f}, x:{x}'
-                #print(tq_str)
+                # tq_str = f'output:{-y:.5f}, re1:{restrain1(x):.3f}, re2:{restrain2(x):.3f}, x:{x}'
+                # print(tq_str)
                 # tq.set_postfix_str(tq_str)
                 # 提供参数的评估结果
+
+                max_target = max(-y, max_target)
                 opt.tell(x, y)
 
         # 获取找到的最佳参数
         best_params = opt.ask()
         tq_str = f'最大值:{-objective(best_params):.5f}, 约束1:{restrain1(best_params):.3f}, 2:{restrain2(best_params):.3f}, 参数:{best_params}'
         print(tq_str)
-        if not restrain1(best_params) > 0 and not restrain2(best_params)> 0:
+        if not restrain1(best_params) > 0 and not restrain2(best_params) > 0 and -objective(best_params) >= max_target:
             break
     print("最佳参数:", best_params)
     print("最大值:", -objective(best_params))
